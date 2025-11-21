45aab2a2901654639d82237267031042e764debe
nikhitagupta <125230948+nikhitagupta@users.noreply.github.com>
2025-11-22 02:08:46 +0530

    Time: 674 ms (60.31%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -2,7 +2,7 @@
 select p.product_name , SUM(o.unit) AS unit 
 from products p
 JOIN orders o
-on o.product_id = p.product_id
-where o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
-group by p.product_name 
-having sum(o.unit)>=100
+on o.product_id=p.product_id
+where o.order_date between '2020-02-01' and '2020-02-29'
+group by p.product_name, p.product_id
+having sum(o.unit)>=100;
